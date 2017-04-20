237afe6fdd9f1d3c14ae8b559187e9a94356bf7b
Jason Liu <jasobs10@gmail.com>
2017-04-20 14:32:30 -0400

    Add dropdown menu to navbar

--- frontend/components/shared/navbar.jsx
+++ frontend/components/shared/navbar.jsx
@@ -3,11 +3,43 @@ import { hashHistory } from 'react-router';
 
 export const Navbar = (props) => {
   return (
-    <div className="nav-wrapper">
-      <nav className="topbar">
-        df
-      </nav>
+    <div className="nav-container">
+      <div className="nav-inside">
 
+        <li className="titlelogo">
+          <i className="fa fa-beer" aria-hidden="true"></i> ONTAP
+        </li>
+        <li>
+          Top beers
+        </li>
+        <li>
+          Breweries
+        </li>
+        <li className="account-button">
+          Account
+          <div className="dropdown">
+            <li>
+              My profile
+            </li>
+            <li>
+              Beer history
+            </li>
+            <li>
+              settings
+            </li>
+            <li>
+              Wish list
+            </li>
+            <li>
+              Log out
+            </li>
+          </div>
+        </li>
+        <li>
+          Log Out
+
+        </li>
+      </div>
     </div>
   );
 };
